dcd4c4e6399b2467efb9543a8f93a63d8e302129
jeffreyhamilton <jvhamilton85@gmail.com>
2019-02-28 17:32:42 -0500

    First commit dealing with HashSets.

--- SetsAndDictionaries/Program.cs
+++ SetsAndDictionaries/Program.cs
@@ -10,6 +10,30 @@ namespace SetsAndDictionaries
     {
         static void Main(string[] args)
         {
+
+            List<Student> students = new List<Student>
+            {
+                new Student() {Name = "Sally" , GradeLevel = 3},
+                new Student() {Name = "Bob" , GradeLevel = 3},
+                new Student() {Name = "Sally" , GradeLevel = 2},
+                new Student() {Name = "Adam" , GradeLevel = 3},
+                new Student() {Name = "Craig" , GradeLevel = 3},
+                new Student() {Name = "Craig" , GradeLevel = 2}
+            };
+
+            SchoolRoll schoolRoll = new SchoolRoll();
+            schoolRoll.AddStudents(students);
+
+            schoolRoll.AddStudents(students);
+
+            foreach(Student student in schoolRoll.Students)
+            {
+                Console.WriteLine($"{student.Name} is in grade {student.GradeLevel}");
+            }
+
+
         }
+
+
     }
 }
---
+++ SetsAndDictionaries/SchoolRoll.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetsAndDictionaries
+{
+    class SchoolRoll
+    {
+        private HashSet<Student> _students = new HashSet<Student>();
+
+        public IEnumerable<Student> Students { get { return _students; } }
+
+        public void AddStudents(IEnumerable<Student> students)
+        {
+            _students.UnionWith(students);
+        }
+
+    }
+}
